dae139197a288f43ee262863b4f29b30840b7f55
adock27 <andersondumet@gmail.com>
2023-01-22 00:55:23 -0500

    Adding a client view

---
+++ src/Form/ServiciosEasyForm.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\nombres\Form;
+
+
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\nombres\Services\Animesdb;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ProyectosEasyForm extends FormBase
+{
+
+
+  public function getFormId(): string
+  {
+    return 'proyectos_easy_form';
+  }
+
+
+  public function buildForm(array $form, FormStateInterface $form_state, $data = []): array
+  {
+
+//    dpm($data, 'data from form');
+
+    $form['nombre'] = array(
+      '#type' => 'textfield',
+      '#title' => $this
+        ->t('Nombre'),
+      '#required' => TRUE,
+      '#default_value' => '',
+      '#attributes' => array('class' => array('mb-3')),
+    );
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => 'Agregar',
+    );
+
+    $form['id'] = array(
+      '#type' => 'hidden',
+      '#value' => $data ? $data : '',
+    );
+
+
+
+    return $form;
+  }
+
+
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+
+  }
+
+
+  public function submitForm(array &$form, FormStateInterface $form_state, $id = NULL)
+  {
+
+
+    $nombre = $form_state->getValue('nombre');
+
+    $node = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->create([
+        'type' => 'proyectos',
+        'title' => $nombre,
+        'field_article_id' => $form_state->getValue('id'),
+        'status' => 0,
+      ]);
+
+//    dpm($node);
+
+    $node->save();
+
+    $nid = $node->id();
+
+    \Drupal::messenger()->addMessage("Se ha agregado correctamente la entidad $nombre en modo borrador");
+    \Drupal::messenger()->addMessage("Se recomienda encarecidamente actualizar los campos y publicar");
+
+//    $form_state->setRedirect('entity.node.canonical', ['node' => $nid]);
+
+
+    $form_state->setRedirectUrl(Url::fromUri('internal:' . "/node/$nid/edit"));
+
+//dpm($nid, 'Las node created by custom form');
+
+
+  }
+
+
+}
+
+
---
+++ src/Plugin/Block/ServiciosEasyFormBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\nombres\Plugin\Block;
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'servicios' Block.
+ *
+ * @Block(
+ *   id = "Bloque de formulario de Proyectos simple",
+ *   admin_label = @Translation("Bloque de formulario de Proyectos simple"),
+ *   category = @Translation("Bloques de formularios custom"),
+ * )
+ */
+
+
+class ProyectosEasyFormBlock extends BlockBase
+{
+
+  /**
+   * Builds and returns the renderable array for this block plugin.
+   *
+   * If a block should not be rendered because it has no content, then this
+   * method must also ensure to return no content: it must then only return an
+   * empty array, or an empty array with #cache set (with cacheability metadata
+   * indicating the circumstances for it being empty).
+   *
+   * @return array
+   *   A renderable array representing the content of the block.
+   *
+   * @see \Drupal\block\BlockViewBuilder
+   */
+  public function build(){
+
+    $nid = '';
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if ($node instanceof \Drupal\node\NodeInterface) {
+      // You can get nid and anything else you need from the node object.
+      $nid = $node->id();
+
+//      dpm($nid, 'get current node iD');
+    }
+
+
+    $formulario = \Drupal::formBuilder()->getForm('\Drupal\nombres\Form\ProyectosEasyForm',$nid);
+
+    $build[] = $formulario;
+    return $build;
+  }
+}
